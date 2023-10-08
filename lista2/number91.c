15ac84474a370b80dc6c57ab616e685c4049a29f
Mach1r0 <segundacontafd@gmail.com>
2023-10-08 01:19:51 -0300

    make the exercise of list 2

--- lista2/number91.c
+++ lista2/number91.c
@@ -10,16 +10,24 @@ int f91(int n) {
 
 int main() {
     int n;
-
+    int number[1000]; // Declare an array
+    int count = 0; // To save the position in the array
+    
     while (1) {
-        scanf("%d", &n);
+        scanf("%d", &n); // Read the number from the user
 
         if (n == 0) {
-            break;
+            break; // Condition to stop the loop
         }
 
-        int result = f91(n);
-        printf("f91(%d) = %d\n", n, result);
+        int result = f91(n); // Calculate f91 for the input
+        number[count] = result; // Store the result in the array    
+        count++; // Increment the count
+    }
+
+    // Print the stored numbers
+    for (int i = 0; i < count; i++) { // Loop through the array
+        printf("f91(%d) = %d\n", number[i], number[i]); // Print the position of the array specified in the loop
     }
 
     return 0;
