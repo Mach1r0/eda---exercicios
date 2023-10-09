9f7e7549a70a06d288346d87f768ba2a72dc7af9
Mach1r0 <segundacontafd@gmail.com>
2023-10-09 00:20:29 -0300

    fix the exercise moveosx

--- lista2/moveosx.c
+++ lista2/moveosx.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char str[100];
-    scanf("%s", str);
+void moveX(char str[], int len) {
+    int xCount = 0;
+    int currentIndex = 0;
 
-    int len = strlen(str); // Get the length of the string
-    int x_found = 1;  // Flag to indicate if 'x' is found initially
-
-    while (x_found) { // Loop until 'x' is not found
-        x_found = 0;  // Reset the flag at the beginning of each loop
-
-        for (int i = 0; i < len; i++) {
-            if (str[i] == 'x') {
-                // Shift characters to the left to overwrite 'x'
-                for (int j = i; j < len - 1; j++) {
-                    str[j] = str[j + 1];
-                }
-
-                // Place 'x' at the end
-                str[len - 1] = 'x';
-
-                // Update the length to account for the moved 'x'
-                len--; 
-
-                // Set the flag to indicate that 'x' was found in this iteration
-                x_found = 1;  
-            }
+    // Count 'x' characters and skip them while copying
+    for (int i = 0; i < len; i++) {
+        if (str[i] != 'x') {
+            str[currentIndex++] = str[i];
+        } else {
+            xCount++;
         }
     }
+    
+    // Fill the end of the string with 'x' characters
+    for (int i = 0; i < xCount; i++) {
+        str[currentIndex++] = 'x';
+    }
+
+    // Null-terminate the string
+    str[currentIndex] = '\0';
+}
+
+int main() {
+    char str[101];
+    scanf("%s", str);
+
+    int len = strlen(str);
+
+    // Call the 'moveX' function
+    moveX(str, len);
 
     printf("%s\n", str);
+
+    return 0;
 }
