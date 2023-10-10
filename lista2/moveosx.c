af2d04389d5c588f7a3efe6db8c8bb8db27abec2
Mach1r0 <segundacontafd@gmail.com>
2023-10-10 11:55:35 -0300

    endx finally complete

--- lista2/moveosx.c
+++ lista2/moveosx.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
-void move_xs(char *str, int i, int j) {
-    // Caso base: se o caractere atual for nulo, terminamos a recursão
-    if (str[i] == '\0') {
-        // Adicione os caracteres 'x' no final
-        for (int k = 0; k < j; k++) {
-            putchar('x');
-        }
-        putchar('\n');
+void moveXsRecursive(char *str, int start, int end) {
+    if (start >= end) {
         return;
     }
 
-    // Se o caractere atual não é 'x', imprima-o
-    if (str[i] != 'x') {
-        putchar(str[i]);
-    } else {
-        // Se o caractere atual é 'x', aumente o contador 'j'
-        j++;
-    }
+    if (str[start] == 'x') {
 
-    // Chame a função recursivamente para o próximo caractere
-    move_xs(str, i + 1, j);
+        memmove(&str[start], &str[start + 1], end - start);
+        str[end] = 'x';
+        moveXsRecursive(str, start, end - 1);
+    } else {
+        moveXsRecursive(str, start + 1, end);
+    }
 }
 
 int main() {
-    char string[1001]; // Suponhamos que a entrada terá no máximo 1000 caracteres
+    char str[1000]; 
 
-    // Receba a entrada do usuário
-    scanf("%s", string);
+    scanf("%s", str);
 
-    // Chame a função recursiva para mover os 'x'
-    move_xs(string, 0, 0);
+    int len = strlen(str);
+
+    moveXsRecursive(str, 0, len - 1);
+
+    printf("%s\n", str);
 
     return 0;
 }
