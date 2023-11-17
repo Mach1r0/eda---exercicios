fa7ddd27eeeb3f715f7ba220e21ade4f94a7910e
Mach1r0 <segundacontafd@gmail.com>
2023-11-16 22:57:54 -0300

    comment bubble sort

--- lista4/bubble.c
+++ lista4/bubble.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
-void bubblesort(int array[], int n){
-    for (int i = 0; i < n-1; i++){
-        for (int j = 0; j < n - i - 1; j++){
-            if (array[j] > array[j+1]){
+void bubblesort(int array[], int n){ // declarate the array and size of the array
+    for (int i = 0; i < n-1; i++){  // loop for reach all the elements of the array
+        for (int j = 0; j < n - i - 1; j++){ // loop for compare the elements of the array
+            if (array[j] > array[j+1]){ // if the element is bigger than the next element, swap them
                 int temp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = temp;
@@ -13,17 +13,14 @@ void bubblesort(int array[], int n){
 }
 
 int main() {
-    int array[1000];
+    int array[1000]; 
     int n = 0;
-    while(scanf("%d", &array[n]) != EOF) {
-        n++;
+    while(scanf("%d", &array[n]) != EOF) { //read the input until the end of the file
+        n++; // count the number of elements
     }
-    bubblesort(array, n);
-    for(int i=0; i < n; i++) {
-        printf("%d", array[i]);
-        if(i < n-1) {
-            printf(" ");
-        }
+    bubblesort(array, n); // call the function  
+    for(int i=0; i < n; i++) { // print the array sorted
+        printf("%d ", array[i]); // print the element
     }
     printf("\n");
     return 0;
